ff9193a1033947122835110fabfb545f38056d58
Clayton <clayton.aa@gmail.com>
2018-01-17 15:48:40 -0300

    Adicionei novamente algumas dependências
    Quando compilava, não estava exibindo as saídas em stdout
    corretamente.

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 from distutils.core import setup
 import py2exe
 
-setup(
-    options = {
-        'py2exe': {
-            'bundle_files': 1, 
-            'compressed': True,
+import sys; sys.argv.append('py2exe')
 
-            'excludes' : ['_ssl', 'doctest', 'pdb', 'unittest', 'difflib', 'inspect', 'msvcr71.dll'],
+py2exe_options = dict(
+                      excludes=['_ssl',  # Exclude _ssl
+                                'pyreadline', 'difflib', 'doctest', 
+                                'optparse', 'pickle', 'calendar', 'pdb', 'inspect', 'unittest'],  # Exclude standard library
+                      dll_excludes=['msvcr71.dll'],  # Exclude msvcr71
+                      compressed=True,  # Compress library.zip
+                      bundle_files=1
+                      )
 
-        },
-    },
-
-    console=[{
-        'script': 'schema_equalizer.py',
-        "icon_resources": [(1, "ello.ico")]
-    }],
-
-    zipfile = None,
-)
-
+setup(name='Schema Equalizer',
+      version='1.0',
+      description='<Description>',
+      author='Clayton A. Alves',
+      options={'py2exe': py2exe_options},
+      console=[{
+          'script': 'schema_equalizer.py',
+          "icon_resources": [(1, "ello.ico")]
+          }],
+      zipfile=None
+      )
